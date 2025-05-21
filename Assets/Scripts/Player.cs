0d82e45edeb1003fdb21099317f22a76c160dc0f
ConsoleWarrior <oleg.minichev@yandex.ru>
2025-05-21 23:04:28 +0300

    1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class Player : MonoBehaviour
 {
+    public float speed;
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        
+
+    }
+    void FixedUpdate()
+    {
+        Moving();
+    }
+    void Moving()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 objPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            objPosition.z = transform.position.z;
+            transform.position = Vector3.MoveTowards(transform.position, objPosition, speed * Time.deltaTime);
+        }
     }
 }
