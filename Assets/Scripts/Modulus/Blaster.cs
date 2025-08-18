5bcb98f45d32c7715263e2b072f9f111d0f9bfef
ConsoleWarrior <oleg.minichev@yandex.ru>
2025-08-18 17:19:42 +0300

    18.08work

---
+++ Assets/Scripts/Modulus/Blaster.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using UnityEngine;
+
+public class Blaster : Guns
+{
+    [SerializeField] Transform gunBarrel1;
+    [SerializeField] Transform gunBarrel2;
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            targets.Add(other.transform);
+            if (targets.Count == 1 && !flag)
+            {
+                StartCoroutine("Fire");
+            }
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            targets.Remove(other.transform);
+        }
+    }
+    IEnumerator Fire()
+    {
+        flag = true;
+        while (targets.Count > 0)
+        {
+            var dist = Vector3.Distance(targets[0].position, transform.position);
+            target = targets[0];
+            if (targets.Count > 1)
+            {
+                foreach (var item in targets)
+                {
+                    if (Vector3.Distance(item.position, transform.position) < dist)
+                    {
+                        target = item;
+                    }
+                }
+            }
+
+            Vector2 direction = (target.position - transform.position).normalized;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            sprite.rotation = Quaternion.Euler(0, 0, angle - 90);
+
+            audioManager.SoundPlay0();
+            InstantiateBullet(gunBarrel1);
+            InstantiateBullet(gunBarrel2);
+            yield return new WaitForSeconds(reload);
+        }
+        flag = false;
+    }
+
+    private void InstantiateBullet(Transform gunBarrel)
+    {
+        GameObject bullet = Instantiate(bulletPrefab, gunBarrel.position, gunBarrel.rotation);
+        bullet.GetComponent<Bullet>().damage = damage;
+        bullet.GetComponent<Rigidbody2D>().AddForce((target.position - gunBarrel.position).normalized * bulletSpeed, ForceMode2D.Impulse); // Придать пуле скорость
+        Destroy(bullet, 2f);
+    }
+}
