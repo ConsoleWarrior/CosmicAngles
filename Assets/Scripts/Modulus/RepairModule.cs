38dbe8599eb523a2730f9f29e43b1cbdd688ab5e
ConsoleWarrior <oleg.minichev@yandex.ru>
2025-09-25 23:16:02 +0300

    home

---
+++ Assets/Scripts/Modulus/RepairModule.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepairModule : Modulus
+{
+    public float bonus;
+    Transform player;
+    List<Cell> cells = new();
+
+    void Start()
+    {
+        ConnectToShip();
+    }
+
+    void OnDestroy()
+    {
+        if (player != null && !player.GetComponent<Player>().isDestroing)
+            DisconnectFromShip();
+    }
+    public void ConnectToShip()
+    {
+        RememberCell();
+        foreach (Cell cell in cells)
+        {
+            cell.UpdateArmor(bonus);
+            //Debug.Log("+updategood+");
+        }
+    }
+    public void DisconnectFromShip()
+    {
+        RememberCell();
+        foreach (Cell cell in cells)
+        {
+            //cell.UpdateArmor(-(bonus));
+            //Debug.Log("-updategood-");
+            StopAllCoroutines();
+        }
+    }
+    void RememberCell()
+    {
+        cells = new();
+        try
+        {
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+            for (int i = 0; i < player.childCount; i++)
+            {
+                var cell = player.GetChild(i).GetComponent<Cell>();
+                if (cell != null)//вместе с ядром
+                {
+                    cells.Add(cell);
+                }
+            }
+        }
+        catch (NullReferenceException) { Debug.Log("поймал нуль эксепшн, все ок"); }
+
+    }
+    
+    public override string GetCharacter()
+    {
+        return "Repair all ship in 1 sec = " + bonus;
+    }
+}
