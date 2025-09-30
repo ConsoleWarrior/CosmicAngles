65c401b942b2064d037a74662a6a0a3e3d648ea7
ConsoleWarrior <oleg.minichev@yandex.ru>
2025-09-30 17:10:26 +0300

    work

--- Assets/Scripts/Enemys/TurelG.cs
+++ Assets/Scripts/Enemys/TurelG.cs
@@ -9,7 +9,6 @@ public class TurelG : MonoBehaviour
 
 
     private bool fireFlag = false;
-    //[SerializeField] GameObject bulletPrefab;
     [SerializeField] float bulletSpeed;
     [SerializeField] float reloadTime;
     [SerializeField] float damage;
@@ -24,15 +23,8 @@ public class TurelG : MonoBehaviour
             player = GameObject.FindGameObjectWithTag("Player").transform;
         }
         catch { Debug.Log("player is not active"); }//Debug.Log("player is not active"); }
-        ////animator = GetComponent<Animator>();
-        //sector = transform.parent.GetComponent<MapSector>();
-        //target = new Vector3(Random.Range(sector.minX, sector.maxX), Random.Range(sector.minY, sector.maxY), 0);
-        //audioManager.a.volume = 0.5f;
         var managerObj = GameObject.Find("PoolManager").GetComponent<PoolManager>();
         gunBulletPool = managerObj.flyBulletPool;
-        //enemyPool = managerObj.burnerPool;
-        //animPool = managerObj.destroyAnimPool;
-
     }
     void FixedUpdate()
     {
@@ -43,7 +35,9 @@ public class TurelG : MonoBehaviour
             if (dist > 0 && dist < atackDistance)
             {
                 fireFlag = true;
-                Atack();
+                //Atack();
+                StartCoroutine(Fire());
+
             }
         }
         else if (dist > atackDistance)
@@ -51,42 +45,23 @@ public class TurelG : MonoBehaviour
             fireFlag = false;
             StopAllCoroutines();
         }
-        //else
-        //{
-        //    fireFlag = false;
-        //    StopAllCoroutines();
-        //}
     }
-    //void OnTriggerEnter2D(Collider2D other)
-    //{
-    //    if (other.gameObject.CompareTag("Cell"))
-    //    {
-    //        if (!fireFlag)
-    //        {
-    //            fireFlag = true;
-    //            Atack();
-    //        }
-    //    }
-    //}
 
     public void Atack()
     {
-        //if (!fireFlag && dist < fireDistance)
-        //{
-        //fireFlag = true;
         StartCoroutine(Fire());
-        //}
-
         Vector2 direction = (player.position - transform.position).normalized;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0, 0, angle - 90);
-        //hpBarCanvas.transform.rotation = Quaternion.Euler(0, 0, 0);
     }
 
     IEnumerator Fire()
     {
         while (true)
         {
+            Vector2 direction = (player.position - transform.position).normalized;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, angle - 90);
             //audioManager.SoundPlay1();
             var bullet = gunBulletPool.Get();
             bullet.transform.position = transform.position;
@@ -96,12 +71,8 @@ public class TurelG : MonoBehaviour
             blt.gunBulletPool = gunBulletPool;
             bullet.transform.localScale = new(0.15f, 0.15f, 1);
             blt.ReturnToPool(2);
-            //GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
             bullet.GetComponent<Rigidbody2D>().AddForce((player.position - transform.position).normalized * bulletSpeed, ForceMode2D.Impulse);
-            //bullet.GetComponent<Bullet>().damage = damage;
-            //Destroy(bullet, 2f);
             yield return new WaitForSeconds(reloadTime);
         }
-        fireFlag = false;
     }
 }
